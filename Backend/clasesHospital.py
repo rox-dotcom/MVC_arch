a78e8d1afde060bd4d623811c14cd172e5a2ece3
Martín Rivera <A01749620@tec.mx>
2025-03-05 15:55:40 -0600

    Actualización de clases

--- Backend/clasesHospital.py
+++ Backend/clasesHospital.py
@@ -1,10 +1,10 @@
-
-
 class Usuario:
     
-    def __init__(self, nombre, role):
+    def __init__(self, nombre, role, correo):
         self.nombre = nombre
         self.role = role
+        self.password = "1234"
+        self.correo = correo
         
     def show_info(self):
         print(f"Nombre: {self.nombre}, Role: {self.role}")
@@ -17,11 +17,38 @@ class Administrador(Usuario):
     def __init__(self, nombre, role):
         super().__init__(nombre, role)
         
-    def gestionar_citas(self):
-        print("Gestionando citas")
+    def gestionar_citas(self, medico):
+        op = int(print("¿Qué desea hacer?\n(1) Ver citas\n(2) Borrar cita\n(3) Modificar cita\n"))
         
-    def gestionar_horarios(self):
-        print("Gestionando horarios")
+        if op == 1:
+            medico.revisarCitas()
+            
+        elif op == 2:
+            index = int(input("Que cita desea borrar?"))
+            medico.citasMedico.delete(index - 1)
+            
+        elif op == 3:
+            index = int(input("Que cita desea modificar?"))
+            print("Cita a modificar:")
+            medico.citasMedico[index - 1].show_info()
+            medico.citasMedico[index - 1].estado = "Pendiente" if (medico.citasMedico[index - 1].estado == "Confirmada")  else "Confirmada"
+        
+    def gestionar_horarios(self, medico):
+        op = int(print("¿Qué desea hacer?\n(1) Ver citas\n(2) Borrar cita\n(3) Modificar horario\n"))
+        
+        if op == 1:
+            medico.revisarCitas()
+            
+        elif op == 2:
+            index = int(input("Que cita desea borrar?"))
+            medico.citasMedico.delete(index - 1)
+            
+        elif op == 3:
+            index = int(input("Que cita desea modificar?"))
+            print("Cita a modificar:")
+            medico.citasMedico[index - 1].show_info()
+            horario = input("Nuevo horario: ")
+            medico.citasMedico[index - 1].horario = horario
         
     def ver_citas(self):
         print("Viendo citas")
@@ -43,32 +70,32 @@ class Enfermera(Usuario):
         
 class Medico(Usuario):
     
-    
     def __init__(self, nombre, role):
         super().__init__(nombre, role)
         self.citasMedico = []
-        
         
     def revisarCitas(self):
         print("Revisando citas de Dr. " + self.get_name())
         
         print("\tLista de citas previas")
-        for cita in self.citasMedico:
+        for index, cita in enumerate(self.citasMedico):
             if cita.get_estado() == "Confirmada":
-                cita.show_info()    
+                print(f"Cita {index + 1}:")
+                cita.show_info()
                 
-        for cita in self.citasMedico:
+        for index, cita in enumerate(self.citasMedico):
             if cita.get_estado() == "Pendiente":
                 self.actualizarEstadosCitas(cita)
                 
         print("\tLista de citas actualizada")
-        for cita in self.citasMedico:
+        for index, cita in enumerate(self.citasMedico):
             if cita.get_estado() == "Confirmada":
-                cita.show_info()   
+                print(f"Cita {index + 1}:")
+                cita.show_info()
             
     
     def actualizarEstadosCitas(self, cita):
-        print("Cita pendiente de " + cita.get_paciente())
+        print("Cita pendiente de " + cita.get_paciente() + " a las " + cita.hora)
         
         op = int(input("(1) Aceptar cita\n(2) Rechazar cita\n"))
         
@@ -77,93 +104,4 @@ class Medico(Usuario):
             print("Cita confirmada")
         else:
             cita.estado = "Rechazada"
-            print("Cita rechazada")
-        
-        
-    def anadirCita(self, cita):
-        self.citasMedico.append(cita)
-        
-        
-class Paciente(Usuario):
-    
-    def __init__(self, nombre, role):
-        super().__init__(nombre, role)
-        
-    def agendarCita(self, medico):
-        print("Agendando cita con doctor: " + medico.get_name())
-        
-        cita = Cita(self.nombre, "12/12/2021", "10:00", "Pendiente", "Consulta general", medico)
-        self.citasPaciente = cita
-        medico.anadirCita(cita)
-        
-    def verEstadoDeCita(self):
-        print("Viendo estado de la cita de " + self.get_name())
-        print(self.citasPaciente.estado)
-        
-class Cita:
-    
-    def __init__(self, paciente, fecha, hora, estado, motivo, medico):
-        self.paciente = paciente
-        self.fecha = fecha
-        self.hora = hora
-        self.estado = estado 
-        self.motivo = motivo
-        self.medico = medico
-        
-    def show_info(self):
-        print(f"Fecha: {self.fecha}, Hora: {self.hora}, Estado: {self.estado}, Motivo: {self.motivo}, Médico: {self.medico.get_name()}")
-        
-    def get_estado(self):
-        return self.estado
-    
-    def get_paciente(self):
-        return self.paciente
-        
-    
-medico1 = Medico("Juan", "Medico")
-paciente1 = Paciente("Pedro", "Paciente")
-enfermera1 = Enfermera("Maria", "Enfermera")
-admin1 = Administrador("Ana", "Administrador")
-
-doctores = []
-citas = []
-pacientes = []
-enfermeras = []
-
-print("INFO FROM USERS")
-print("_"*30)
-
-medico1.show_info()
-doctores.append(medico1)
-
-paciente1.show_info()
-pacientes
-
-enfermera1.show_info()
-enfermeras
-
-admin1.show_info()
-
-
-print("_"*30)
-print("\n"*3)
-print("AGENDAR CITA DE " + paciente1.get_name())
-print("_"*30)
-
-
-
-paciente1.agendarCita(medico1)
-print("_"*30)
-print("\n"*3)
-print("REVISAR CITAS")
-print("_"*30)
-
-medico1.revisarCitas()
-print("_"*30)
-print("\n"*3)
-
-print("VER ESTADO DE CITA")
-print("_"*30)
-paciente1.verEstadoDeCita()
-print("_"*30)
-print("\n"*3)
+       
